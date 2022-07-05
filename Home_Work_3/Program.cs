c7c5f62f2e40b0e0232c21f5e16863cdff35412d
MishaGaer <gaer1997@yandex.ru>
2022-07-05 12:49:29 +0200

    Add_HW3

---
+++ Home_Work_3/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Напишите программу, 
+которая задаёт массив из 8 элементов 
+и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+//рандомное заполнение массива
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(0, 500);
+        index++;
+    }
+}
+//Печать массива в консоль
+void PrintArray(int[] array)
+{
+    int lenght = array.Length;
+    lenght = lenght - 1;
+    int position = 0;
+    while (position < lenght)
+    {
+        Console.Write($"{array[position]}, ");
+        position++;
+    }
+    Console.WriteLine($"{array[position]}");
+}
+
+//Основной код
+
+int length = 8;
+int[] collection = new int[length];
+
+FillArray(collection);
+PrintArray(collection);
+
+
